90ed6f6e04a65c6d8c1d10f539c5b7223eeb834a
Sadi Muhammad Tanzim <saditanzim@gmail.com>
2025-07-22 21:10:06 +0600

    project initialized

---
+++ config/db.js
@@ -0,0 +1,14 @@
+require('dotenv').config();
+const mongoose = require('mongoose');
+
+const connectDB = async () => {
+    try {
+        await mongoose.connect(process.env.mongoURL);
+        console.log('mongoDB connected')
+    } catch {
+        console.error('❌ MongoDB connection failed:', error.message);
+        process.exit(1);
+    }
+};
+
+module.exports = connectDB;
