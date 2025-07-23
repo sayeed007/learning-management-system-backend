784760d75c2e5983908e4fbc58b3413806163bd3
Sadi Muhammad Tanzim <saditanzim@gmail.com>
2025-07-23 10:42:23 +0600

    register api added

--- app.js
+++ app.js
@@ -1,8 +1,14 @@
 const express = require('express');
+const { registerRouter } = require('./routes/register.route');
 const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
 app.get('/', (req, res) => {
     res.send('project is running');
 });
+
+app.use(registerRouter);
 
 module.exports = app;
---
+++ routes/register.route.js
@@ -0,0 +1,37 @@
+const express = require('express');
+const bcrypt = require('bcrypt');
+const { User } = require('../models/users.model');
+const router = express.Router();
+
+router.get('/register', (req, res) => {
+    res.send('register route');
+});
+
+router.post('/register', async (req, res) => {
+    const { name, email, password } = req.body;
+    const SALT_ROUNDS = parseInt(process.env.SALT_ROUNDS);
+    try {
+        const existingUser = await User.findOne({ email });
+        if (existingUser) {
+            return res.status(400).send({ message: 'User already exist' });
+        };
+
+        const hashedPassword = await bcrypt.hash(password, SALT_ROUNDS);
+
+        const newUser = new User({ name, email, password: hashedPassword });
+        const savedUser = await newUser.save();
+        return res.status(201).send({
+            message: 'User registered successfully',
+            user: {
+                id: savedUser._id,
+                name: savedUser.name,
+                email: savedUser.email
+            }
+        });
+
+    } catch (error) {
+        return res.status(500).send({ message: error.message });
+    }
+});
+
+module.exports = { registerRouter: router };
